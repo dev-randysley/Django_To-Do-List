50a86750c71ed88bbc712ab09eeaf9797f6c3980
Randy Sleyker <63586427+dev-randysley@users.noreply.github.com>
2021-12-10 15:04:56 -0300

    Delete webempresa directory

--- webempresa/servicios/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from .models import servicios
-# Create your views here.
-
-def Servicios(request):
-	# cargamos todos los registro de nuestra base de datos como objetos y pasamos un diccionario con ellos
-	lista_servicios = servicios.objects.all()
-	return render(request,"servicios/servicios.html",{'servicios':lista_servicios})
