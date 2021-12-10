808c383aff053d10d5910f2ed7b770f3d90d924f
Randy Sleyker <63586427+dev-randysley@users.noreply.github.com>
2021-12-10 15:05:08 -0300

    Delete webplayground directory

--- webplayground/registration/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-# Register your models here.
-
-class RegistrationAdmin(admin.ModelAdmin):
-    list_display = ('user',)        
-
-
-admin.site.register(Profile,RegistrationAdmin)
